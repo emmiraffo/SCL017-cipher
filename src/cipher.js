95b690edc36458e36e07c9aa288464bde9f0ce58
Emmi Raffo <emmyraffoc@gmail.com>
2021-05-15 15:05:48 -0400

    Realice la funcion de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
 const cipher = {
-  // ...
-};
+    "codificar": function (desplazamiento,palabra){
+        let unionLetras ="" ;
+        for (let i=0; i < palabra.length ; i++  ) {
+            let codigoAsci = palabra.charCodeAt(i)
+            let letraCodificada =  ((codigoAsci - 65 + desplazamiento) % 26) + 65
+            unionLetras  = unionLetras + String.fromCharCode(letraCodificada) 
+      }
+     return unionLetras
+    },
+    "decodificar": function (desplazamiento,palabra){
+        for (let i=0 ; i<palabra.length; i++) {
+            let posicionAsci = palabra.fromCharCode(i)
+            console.log (posicionAsci)
+        }
+            //let letraDecodificada = ()
 
-export default cipher;
+
+        }
+       
+    }
+    
+
+//letras.charCodeAt(0)
+
+//String.fromCharCode(72)
+
+export default cipher
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+
+
+// Funcion  para pasar de una caja a otra por el evento onclick
+
+
+ function inicioCifrado() {
+var texto = document.getElementById("cajaT1").value;
+texto= texto.toUpperCase()
+var offset = document.getElementById("offset").value;
+offset = Number(offset)
+var textoCifrado = cipher.codificar(offset,texto) 
+document.getElementById("cajaT2").innerHTML= textoCifrado;
+ }
+ 
+function inicioDecifrado() {
+    var texto = document.getElementById("cajaT1").value;
+    texto= texto.toUpperCase()
+    var offset = document.getElementById("offset").value;
+    offset = Number(offset)
+    var textoDecifrado = cipher.decodificar(offset,texto) 
+    document.getElementById("cajaT2").innerHTML= textoDecifrado;
+
+}
+window.inicioCifrado = inicioCifrado
+window.inicioDecifrado = inicioDecifrado
