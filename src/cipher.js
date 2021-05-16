bc889406de9ff0d5e7c1fe9762852f52d07c8d83
Emmi Raffo <emmyraffoc@gmail.com>
2021-05-16 09:55:38 -0400

    realice la condicion para que dejara el espacio como espacio

--- src/cipher.js
+++ src/cipher.js
@@ -2,8 +2,12 @@ const cipher = {
     "codificar": function (desplazamiento,palabra){
         let unionLetras ="" ;
         for (let i=0; i < palabra.length ; i++  ) {
+            console.log(palabra[i])
             let codigoAsci = palabra.charCodeAt(i)
             let letraCodificada =  ((codigoAsci - 65 + desplazamiento) % 26) + 65
+            if (codigoAsci === 32) {
+                letraCodificada = 32
+            }
             unionLetras  = unionLetras + String.fromCharCode(letraCodificada) 
       }
      return unionLetras
@@ -13,6 +17,9 @@ const cipher = {
         for (let i=0 ; i < palabra.length ; i++) {
             let posicionAsci = palabra.charCodeAt(i)
             let ubicacionAlfabeto = ((posicionAsci +65 - desplazamiento) % 26) + 65
+            if (posicionAsci === 32) {
+                ubicacionAlfabeto = 32
+            }
             unionLetras = unionLetras + String.fromCharCode(ubicacionAlfabeto)
             
         }
