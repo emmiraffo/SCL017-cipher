280b48918d7da41471bc631f65ffbab096b608c1
Emmi Raffo <emmyraffoc@gmail.com>
2021-07-08 22:07:17 -0400

    modificaciones de estilo y sintaxis de funciones

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ function inicioCifrado() {
     offset = Number(offset)
     var textoCifrado = cipher.encode(offset,texto) 
     document.getElementById("cajaT2").innerHTML= textoCifrado;
+    
  }
 
  // Función asignada al botón de descifrado, tom el offset y lo convierte en entero, y el contenido de la caja 1 para realizar la función de descifrar
@@ -19,24 +20,49 @@ function inicioDescifrado() {
     offset = Number(offset)
     var textoDecifrado = cipher.decode(offset,texto) 
     document.getElementById("cajaT2").innerHTML= textoDecifrado;
+    
 }
 
 function accionFinal1() {
-   document.getElementById("mensaje").innerHTML = "Tu mensaje ya está cifrado"  
+   document.getElementById("mensaje").innerHTML = "Mensaje Cifrado"  
 }
 function accionFinal2() {
-    document.getElementById("mensaje").innerHTML = "Tu mensaje ya está Descifrado"  
+    document.getElementById("mensaje").innerHTML = "Mensaje Descifrado"  
  }
 
+function showBox () {
+    document.getElementById("boxTwo").classList.toggle("show");
+    
+}
 
 
 
-var botonCifrado = document.getElementById("cifrado");
-var botonDescifrado = document.getElementById("descifrado");
 
+var botonCifrado = document.getElementById("buttonEncode");
+botonCifrado.addEventListener("click", ()=> {
+    var texto = document.getElementById("cajaT1").value;
+    var offset = document.getElementById("offset").value;
+    if (texto == 0)
+    return alert ("Introduce el Texto que deseas Cifrar");
+    else if (offset == ""){
+        return alert ("Introduce el Desplazamiento que deseas");
+    }
+    else
+    inicioCifrado();
+    showBox();
+    accionFinal1();
+    });
 
-
-botonCifrado.addEventListener("click", inicioCifrado)
-botonCifrado.addEventListener("click", accionFinal1)
-botonDescifrado.addEventListener("click", inicioDescifrado)
-botonDescifrado.addEventListener("click", accionFinal2)
+var botonDescifrado = document.getElementById("buttonDecode");
+botonDescifrado.addEventListener("click", () => {
+    var texto = document.getElementById("cajaT1").value;
+    var offset = document.getElementById("offset").value;
+    if (texto == 0){
+        return alert ("Introduce el texto que deseas Descifrar");
+    } else if (offset == ""){
+            return alert ("Introduce el Desplazamiento que deseas");
+    } else
+        inicioDescifrado ();
+        showBox();
+        accionFinal2();
+ } );
